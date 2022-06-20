3aaf2d2147c24bd01d9d3ee40a2c707fd632cd81
danushika.priyadarshani <danushika@ideahub.lk>
2022-06-20 21:47:53 +0530

    Initial Commit

---
+++ ContactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Danushika Priyadarshani on 2022-06-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
